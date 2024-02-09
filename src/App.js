ecc307addda11eec0ac83d1a61c81c58da50fbc9
kangjs3779 <kangjs3779@naver.com>
2024-02-09 18:55:52 +0900

    props 구조 분해 할당

--- src/App.js
+++ src/App.js
@@ -3,20 +3,17 @@ function App() {
 
   return (
     <>
-      <MyComp title="내 컴포넌트" name="흥민" />
-      <MyComp title="I am 컴포넌트에요" name="강인" />
+      <MyComp value="흥민" address="신촌" />
+      <MyComp value="강인" address="강남" />
     </>
   );
 }
 
-//함수 인잘
-// 사용한 곳에서 넘긴 property들이 객체 형태로 담김
-// 예) title="내 콤포넌트" => {title: "내콤포넌트"}
-function MyComp(property) {
-  console.log(property);
+function MyComp({value, address}) {
+  
   return (
     <div>
-      <h1 title={property.title}>hello {property.name}</h1>
+      <h1>{value}는 {address}에 산다.</h1>
     </div>
   )
 }
